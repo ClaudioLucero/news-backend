43d31946c13bbb932d37b19f4fe84f718f6b1f12
cluceroLaVoz <clucero@lavozdelinterior.com.ar>
2024-10-16 10:47:20 -0300

    config docker

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+# Usa la imagen base de Node.js
+FROM node:19
+
+# Establece el directorio de trabajo en el contenedor
+WORKDIR /usr/src/app
+
+# Copia package.json y package-lock.json al contenedor
+COPY . .
+
+# Instala las dependencias de la aplicación
+RUN npm install 
+
+# Copia el resto de los archivos de la aplicación al contenedor
+COPY . .
+
+# Expone el puerto en el que la aplicación estará escuchando
+EXPOSE 3000
+
+# Comando para ejecutar la aplicación
+CMD ["npm", "start"]
--- app.js
+++ app.js
@@ -7,16 +7,26 @@ import errorHandler from './middlewares/errorHandler.js';
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors()); // Habilitar CORS para todas las rutas
-
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URI);
+// Conectar a MongoDB
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => {
+    console.log('Conectado a MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error al conectar a MongoDB:', error);
+  });
 
-// app.use('/news', newsRoutes);
+// Usar las rutas
 app.use(routes);
+
 // Usar el middleware de manejo de errores
 app.use(errorHandler);
 
